36f0f1faeb4597816aa0d3135b3115c6db2bada2
Tealeda <tealedacoder@gmail.com>
2015-01-30 15:28:19 -0500

    generate random number; accept/append user guess

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 
 $(document).ready(function(){
+	randomNumber();
+	thisGame();
+	newGame();
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -13,5 +16,38 @@ $(document).ready(function(){
   	});
 
 });
+
+//Generate random number
+var x = Math.floor((Math.random() * 101) + 1);
+function randomNumber(){
+	console.log("random number: " + x);
+}
+
+//Current game
+function thisGame(){
+	$(".button").click(function(e){
+		e.preventDefault();
+
+		//Add guess to list
+		$("#guessList").append('<li>' + ($("#userGuess").val()) + '</li>');
+
+		//Hot or cold prompts
+		if (($("#userGuess").val()) > x ) { ($("h2#feedback").text("Greater")); }
+		else { ($("h2#feedback").text("Lesser")); }
+
+		//Number of attempts
+		$('#count').html(function(i, val){ 
+			return val*1+1 
+		});
+	});
+}
+
+
+//Refresh page; start new game
+function newGame(){
+	$('.new').click(function(){
+		location.reload();
+	})
+}
 
 
