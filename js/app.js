2b9512dd31f96458ee0a60060cda01d713bd3a76
Tealeda <tealedacoder@gmail.com>
2015-01-30 20:21:48 -0500

    Change hot and cold prompts to absolute range

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	randomNumber();
 	thisGame();
@@ -27,18 +26,52 @@ function randomNumber(){
 function thisGame(){
 	$(".button").click(function(e){
 		e.preventDefault();
+		var guess =  $('#userGuess').val();
+			guess =  Number(guess);
 
-		//Add guess to list
-		$("#guessList").append('<li>' + ($("#userGuess").val()) + '</li>');
+			//Check if guess is a string
+			if (guess != Number(guess)) {
+				alert("Enter number");
+				$('#userGuess').val(''); //clear input field
+			}
 
-		//Hot or cold prompts
-		if (($("#userGuess").val()) > x ) { ($("h2#feedback").text("Greater")); }
-		else { ($("h2#feedback").text("Lesser")); }
+			//Check if guess is larger than 100 or smaller than 1
+			else if (guess > 100 || guess < 1){
+				alert("Enter number between 1-100");
+				$('#userGuess').val(''); //clear input field
+			}
+			else {
+				console.log(guess);
+				
+				//Add guess to list
+				$("#guessList").append('<li>' + ($("#userGuess").val()) + '</li>');
 
-		//Number of attempts
-		$('#count').html(function(i, val){ 
-			return val*1+1 
-		});
+				//Hot or cold prompts
+				if (guess == x){
+					$("h2#feedback").text("Correct!");
+				}
+				if (guess > x + 50 || guess < x + 50 ){ 
+					$("h2#feedback").text("Ice Cold"); 
+					$('#userGuess').val('');
+				}
+				if (guess > x + 49 || guess < x + 49 || guess > x + 30 || guess < x + 30 ){ 
+					$("h2#feedback").text("Cold"); 
+					$('#userGuess').val('');
+				}
+				if (guess > x + 29 || guess < x + 29 || guess > x + 10 || guess < x + 10 ){ 
+					$("h2#feedback").text("Warm"); 
+				}
+				if (guess > x + 9 || guess < x + 9 || guess > x + 1 || guess < x + 1 ){ 
+					$("h2#feedback").text("Hot");
+				} 
+
+				//Count number of attempts
+				$('#count').html(function(i, val){ 
+					return val*1+1 
+				});
+
+				$('#userGuess').val(''); //clear input field
+			}
 	});
 }
 
@@ -49,5 +82,6 @@ function newGame(){
 		location.reload();
 	})
 }
+
 
 
